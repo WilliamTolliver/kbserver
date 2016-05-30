86592f97686afc000bc47093d3226d5fa53d8573
Bludbathmcgrath <William_R_Tolliver@yahoo.com>
2016-05-30 06:21:20 -0400

    Migrated to an H2database. Added JPA data access structure.

---
+++ src/main/java/com/wttechnologies/spring/test/myproject/UserRepository.java
@@ -0,0 +1,16 @@
+package com.wttechnologies.spring.test.myproject;
+
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+	List<User> findById(int id);
+    List<User> findByfirstName(String firstName);
+    List<User> findByLastName(String lastName);
+    List<User> findByEmail(String email);
+    List<User> findByUsername(String username);
+    List<User> findByPassword(String password);
+}
