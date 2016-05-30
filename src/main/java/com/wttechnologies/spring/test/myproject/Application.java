b2e9eb400ab435a5b506645f2c07fb024c5e30f1
Bludbathmcgrath <William_R_Tolliver@yahoo.com>
2016-05-29 21:49:30 -0400

    Added Hibernate ORM, mysql database connection, and User database table with test record insert.

--- src/main/java/com/wttechnologies/spring/test/myproject/Application.java
+++ src/main/java/com/wttechnologies/spring/test/myproject/Application.java
@@ -1,20 +1,42 @@
 package com.wttechnologies.spring.test.myproject;
+
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+import com.wttechnologies.spring.test.myproject.User;
+import com.wttechnologies.spring.test.util.*;
+
+import org.hibernate.*;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
-    }
-    
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
+	public static void main(String[] args) throws Exception {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+
+		session.beginTransaction();
+
+
+		session.save(new User("William", "Tolliver", "Wtolliver@Nowhere.com", "Wtolliver", "Password" ));
+
+		session.getTransaction().commit();
+
+		Query q = session.createQuery("From User ");
+
+		List<User> resultList = q.list();
+		System.out.println("num of employess:" + resultList.size());
+		for (User next : resultList) {
+			System.out.println("next User: " + next);
+		}
+	}
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 }
---
+++ src/main/java/com/wttechnologies/spring/test/myproject/User.java
@@ -0,0 +1,108 @@
+package com.wttechnologies.spring.test.myproject;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class User {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String username;
+	private String password;
+	
+	public User(String firstName, String lastName, String email, String username, String password){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.username = username;
+		this.password = password;
+	}
+	
+	/**
+	 * @return the id
+	 */
+	@Id
+    @GeneratedValue
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", username=" + username + ", password=" + password + "]";
+	}
+	
+
+	
+}
