cd2fb0ad980b96bcb35ef9cc9caf35ea9033a76e
Bludbathmcgrath <William_R_Tolliver@yahoo.com>
2016-05-30 06:20:26 -0400

    Migrated to an H2 database.

--- src/main/java/com/wttechnologies/spring/test/myproject/Application.java
+++ src/main/java/com/wttechnologies/spring/test/myproject/Application.java
@@ -1,42 +1,38 @@
 package com.wttechnologies.spring.test.myproject;
 
-import java.util.concurrent.atomic.AtomicLong;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
-
-import com.wttechnologies.spring.test.myproject.User;
-import com.wttechnologies.spring.test.util.*;
-
-import org.hibernate.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) throws Exception {
-		Session session = HibernateUtil.getSessionFactory().openSession();
-
-		session.beginTransaction();
-
-
-		session.save(new User("William", "Tolliver", "Wtolliver@Nowhere.com", "Wtolliver", "Password" ));
-
-		session.getTransaction().commit();
-
-		Query q = session.createQuery("From User ");
-
-		List<User> resultList = q.list();
-		System.out.println("num of employess:" + resultList.size());
-		for (User next : resultList) {
-			System.out.println("next User: " + next);
-		}
+		SpringApplication.run(Application.class, args);
 	}
 
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
+	@Bean
+	public CommandLineRunner demo(UserRepository repository) {
+		return (args) -> {
+			// save a couple of customers
+			repository.save(new User("Jack", "Bauer", "", "", ""));
+			repository.save(new User("Chloe", "O'Brian", "", "", ""));
+			repository.save(new User("Kim", "Bauer", "", "", ""));
+
+			// fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			for (User customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+
+		};
 	}
 
 }
--- src/main/java/com/wttechnologies/spring/test/myproject/User.java
+++ src/main/java/com/wttechnologies/spring/test/myproject/User.java
@@ -11,90 +11,114 @@ public class User {
 	private String email;
 	private String username;
 	private String password;
-	
-	public User(String firstName, String lastName, String email, String username, String password){
+
+	public User(String firstName, String lastName, String email, String username, String password) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.username = username;
 		this.password = password;
 	}
-	
+
+	public User() {
+
+	}
+
 	/**
 	 * @return the id
 	 */
 	@Id
-    @GeneratedValue
+	@GeneratedValue
 	public int getId() {
 		return id;
 	}
+
 	/**
-	 * @param id the id to set
+	 * @param id
+	 *            the id to set
 	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	/**
 	 * @return the firstName
 	 */
 	public String getFirstName() {
 		return firstName;
 	}
+
 	/**
-	 * @param firstName the firstName to set
+	 * @param firstName
+	 *            the firstName to set
 	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	/**
 	 * @return the lastName
 	 */
 	public String getLastName() {
 		return lastName;
 	}
+
 	/**
-	 * @param lastName the lastName to set
+	 * @param lastName
+	 *            the lastName to set
 	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	/**
 	 * @return the email
 	 */
 	public String getEmail() {
 		return email;
 	}
+
 	/**
-	 * @param email the email to set
+	 * @param email
+	 *            the email to set
 	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	/**
 	 * @return the username
 	 */
 	public String getUsername() {
 		return username;
 	}
+
 	/**
-	 * @param username the username to set
+	 * @param username
+	 *            the username to set
 	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	/**
 	 * @return the password
 	 */
 	public String getPassword() {
 		return password;
 	}
+
 	/**
-	 * @param password the password to set
+	 * @param password
+	 *            the password to set
 	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
@@ -102,7 +126,5 @@ public class User {
 		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
 				+ ", username=" + username + ", password=" + password + "]";
 	}
-	
 
-	
 }
